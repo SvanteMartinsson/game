c19dcb0a317c1b741ca6f2ae90b1d4479b9c4d8b
SvanteMartinsson <svante116360@gmail.com>
2018-03-27 19:38:10 +0200

    real intial upload

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,84 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 
-public class Main {
+public class Main extends Canvas{
+	
+	
+	
+	// Window variables
+	static final int WIDTH = 1024;
+	static final int HEIGHT = 768;
+	static final String title = "Game";
+
+
+	
+	boolean isRunning = false;
+	
+	private BufferStrategy buffer;
+	
+	public Main(){
+		isRunning = true;
+			
+		Window window = new Window(WIDTH, HEIGHT, title, this);
+		this.createBufferStrategy(3);
+		buffer = this.getBufferStrategy();
+		gameLoop();
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello adam and svanteeee e snel");
+		new Main();
+	}
+	
+	public void gameLoop(){
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(isRunning){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
 
+			while(delta >=1){
+				update();
+				delta--;
+			}
+
+			if(isRunning){
+				render();
+
+			}
+
+			frames++;
+			
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: "+ frames);
+				frames = 0;
+			}
+			
+			
+		}
+	}
+	
+	public void render(){
+		
+		Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
+
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		buffer.show();
+	}
+	
+	public void update(){		
+		
 	}
 
 }
