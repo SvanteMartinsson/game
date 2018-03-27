b504ef6ed4d5e3f52435b718cb985e31bbc52801
lucas rehn <lucasr1234@hotmail.com>
2018-03-27 20:15:32 +0200

    object klassen

---
+++ src/object.java
@@ -0,0 +1,14 @@
+import java.awt.Image;
+
+public abstract class object {
+	
+	
+	protected Image image;
+
+	protected double x, y; 
+	
+	public abstract void render();
+	
+	public abstract void update();
+	
+}
