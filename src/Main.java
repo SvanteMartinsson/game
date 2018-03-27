e88ae95bfd62776a84a5406fc67dd5c99331d7b5
lucas rehn <lucasr1234@hotmail.com>
2018-03-27 19:20:38 +0200

    bog

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello adam and svanteeee e snel");
+		System.out.println("Hello adam and svanteeee e bög");
 
 	}
 
